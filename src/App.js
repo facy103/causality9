ee3eb81ce2476aff4f0699fa6e8671192bfe4b55
Mathew Mozer <zhydian@gmail.com>
2020-03-15 14:20:07 -0400

    Added Request Animation Frame wait for the window scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from 'react';
-import {Motion, spring} from 'react-motion';
+import React, { useState, useEffect } from 'react';
+import { Motion, spring } from 'react-motion';
 import './style.css';
 import Footer from "./Components/Footer/Footer.js"
 import Prompt from "./Components/Prompt/Prompt.js"
@@ -7,9 +7,9 @@ import PromptInput from "./Components/Horizontal/PromptInput.js"
 import SummaryInner from "./Components/Prompts/SummaryInner.js"
 import questions from './DB/perfectDB.js';
 // import questions from './DB/causality.js';
-import {findTitle, textTransform} from './mytools.js';
+import { findTitle, textTransform } from './mytools.js';
 
-let questionCount = 0 ;
+let questionCount = 0;
 let questionCountStore = [0];
 let wishDB = [];
 let newOutput = [questions[0]];
@@ -21,24 +21,23 @@ const bottomOfScreen = React.createRef();
 
 const App = () => {
 
-questions[0].question = textTransform(questions, questions[0], questions[0].question, false);
-const [terminalOutput,setTerminalOutput] = useState([questions[0]]);
-const [scrollTop, setScrollTop] = useState(0)
+  questions[0].question = textTransform(questions, questions[0], questions[0].question, false);
+  const [terminalOutput, setTerminalOutput] = useState([questions[0]]);
+  const [scrollTop, setScrollTop] = useState(0)
 
-const scrollBottom = () => {
-  let chatDivHeight = chatDivRef.current?.scrollHeight;
-  if (chatDivHeight > 0 && chatDivHeight !== scrollTop) {
-    setScrollTop(chatDivHeight)
-  }
-}
-
-const txtSubmit = (e, ph) => {
-      questions[questionCount].answer = e;
-      setNextStep();
+  const scrollBottom = () => {
+    let chatDivHeight = chatDivRef.current?.scrollHeight;
+    if (chatDivHeight > 0 && chatDivHeight !== scrollTop) {
+      setScrollTop(chatDivHeight)
+    }
   }
 
-  const setIndex = (value) =>
-  {
+  const txtSubmit = (e, ph) => {
+    questions[questionCount].answer = e;
+    setNextStep();
+  }
+
+  const setIndex = (value) => {
     let currentQ = questions[questionCount];
 
     if ("wishDBUpdate" in currentQ) {
@@ -46,103 +45,104 @@ const txtSubmit = (e, ph) => {
     }
 
     if ("swap" in currentQ) {
-        for ( let swapRef in currentQ.swap )
-        {     
-          var swapObj = questions[questionCount].swap ;
-          var sourceValue = findTitle(questions, swapRef);
-          var targetValue = findTitle(questions, swapObj[swapRef] );
-      
-          questions[sourceValue].answer = questions[targetValue].answer ;
-        }
+      for (let swapRef in currentQ.swap) {
+        var swapObj = questions[questionCount].swap;
+        var sourceValue = findTitle(questions, swapRef);
+        var targetValue = findTitle(questions, swapObj[swapRef]);
+
+        questions[sourceValue].answer = questions[targetValue].answer;
+      }
     }
 
-    if ("clear" in currentQ ) {
+    if ("clear" in currentQ) {
       window.location.reload(false);
     }
 
-    if ('goto' in currentQ ) {
-      questionCount = findTitle (questions,currentQ.goto);
+    if ('goto' in currentQ) {
+      questionCount = findTitle(questions, currentQ.goto);
       return;
     }
 
     if ('next' in currentQ) {
-      questionCount = findTitle (questions, currentQ.next[value]);
+      questionCount = findTitle(questions, currentQ.next[value]);
       return;
-    }  
+    }
 
-    questionCount ++ ;
+    questionCount++;
   }
 
-  const setNextStep=(value)=> {
+  const setNextStep = (value) => {
     setIndex(value);
-    let obj = questions[questionCount]; 
+    let obj = questions[questionCount];
     let mirror = true;
-    if ( obj.mirror === false ) { mirror=false; }
+    if (obj.mirror === false) { mirror = false; }
     questionCountStore.push(questionCount);
     newOutput = [...terminalOutput];
-    newOutput.push({...questions[questionCount]});
-    newOutput[newOutput.length-1].question = textTransform(questions, obj, obj.question, mirror);
-    setTimeout(() => {setTerminalOutput( newOutput)}, 700);
+    newOutput.push({ ...questions[questionCount] });
+    newOutput[newOutput.length - 1].question = textTransform(questions, obj, obj.question, mirror);
+    setTimeout(() => { setTerminalOutput(newOutput) }, 700);
   }
 
-  const generateStep =()=> {
-    let returnedObject = terminalOutput.map ( ( obj, index )=> {
-         switch ( obj.dialogeType ) {
+  const generateStep = () => {
+    let returnedObject = terminalOutput.map((obj, index) => {
+      switch (obj.dialogeType) {
         case "prompt":
-            return <Prompt
+          return <Prompt
             key={index}
             index={index}
             botMessege={obj.question}
-            setNextStep={(value)=>setNextStep(value)}
+            setNextStep={(value) => setNextStep(value)}
             obj={obj}
             terminalOutput={terminalOutput}
-            />
-        case "horizontal":  
-            return <PromptInput 
+          />
+        case "horizontal":
+          return <PromptInput
             key={index}
             index={index}
-            botMessege={obj.question}     
-            txtSubmit={(e, ph)=>txtSubmit(e, ph)}
+            botMessege={obj.question}
+            txtSubmit={(e, ph) => txtSubmit(e, ph)}
             onChange={scrollBottom}
             ph={obj.placeholder}
             obj={obj}
             terminalOutput={terminalOutput}
-            />  
+          />
         case "summaryInner":
-            return <SummaryInner 
+          return <SummaryInner
             key={index}
             index={index}
             botMessege={obj.question}
             h1={obj.h1}
-            setNextStep={(value)=>setNextStep(value)}
+            setNextStep={(value) => setNextStep(value)}
             wishDB={wishDB}
             obj={obj}
             terminalOutput={terminalOutput}
-            />
-          default:
-            break;
-        }
-      })
-      scrollBottom();
-      return returnedObject
-    }
-
-    useEffect(() => {
-      bottomOfScreen.current.scrollIntoView({ behavior: "smooth" },[scrollTop])
+          />
+        default:
+          break;
+      }
     })
-    
-    return (
-        <div id="mypage">
-        <div  id="chatContainer">
-      
-            <div ref={chatDivRef} id="chatDiv"> 
-              {generateStep()}
-              <div ref={bottomOfScreen} style={{ backgroundColor:'red', float: "left", clear: "both" }}>bottomOfScreen</div>
-            </div>
-        </div>  
-        <Footer />
+    scrollBottom();
+    return returnedObject
+  }
+
+  useEffect(() => {
+    window.requestAnimationFrame(() => { // wait just a bit
+      bottomOfScreen.current.scrollIntoView({ behavior: "smooth" }, [scrollTop])
+    });
+  })
+
+  return (
+    <div id="mypage">
+      <div id="chatContainer">
+
+        <div ref={chatDivRef} id="chatDiv">
+          {generateStep()}
+          <div ref={bottomOfScreen} style={{ backgroundColor: 'red', float: "left", clear: "both" }}>bottomOfScreen</div>
+        </div>
+      </div>
+      <Footer />
     </div>
-)
+  )
 }
 
 export default App;
